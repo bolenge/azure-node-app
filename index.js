6595a95bd52945fe7b2639ee00dacf704f2fbff1
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2025-06-18 15:21:22 +0100

    feat: update pages

--- index.js
+++ index.js
@@ -11,14 +11,21 @@ const { readJsonBlob } = require('./utils/blobService');
 // Définir EJS comme moteur de rendu
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Route principale
 app.get('/', async (req, res) => {
+  res.render('index');
+});
+
+app.get('/formations', async (req, res) => {
   try {
     const formations = await readJsonBlob('formations.json');
-    res.render('index', { formations });
+
+    res.render('formations', { formations });
   } catch (err) {
     console.error('Erreur lecture formations:', err.message);
+
     res.status(500).send('Erreur chargement des formations');
   }
 });
