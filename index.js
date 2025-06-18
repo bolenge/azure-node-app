559fe9cfe0013848fc7ae94b37d708d678f64789
Don de Dieu Bolenge <dondedieubolenge@gmail.com>
2025-06-18 11:05:31 +0100

    feat: integrate ejs pages

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Définir EJS comme moteur de rendu
+app.set('view engine', 'ejs');
+
+// Route principale
 app.get('/', (req, res) => {
-  res.send('Hello World depuis Azure !');
+  res.render('index');
 });
 
+// Lancer le serveur
 app.listen(PORT, () => {
-  console.log(`Serveur lancé sur http://localhost:${PORT}`);
+  console.log(`Serveur lancé sur le port ${PORT}`);
 });
